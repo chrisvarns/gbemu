e81afb17e5bdd83e67b8bf5154ba9b3a1fb5b247
Chris Varnz <chrisvarnz@gmail.com>
2019-06-01 20:24:39 +0100

    Made a dent in fleshing out the pixel fifo and tile fetch

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -4,6 +4,9 @@
 #include "ppu.h"
 #include "utils.h"
 
+#include <cassert>
+#include <queue>
+
 enum class PPU_STATE
 {
 	DISABLED,
@@ -13,9 +16,8 @@ enum class PPU_STATE
 	VBLANK
 };
 
-const int PIXEL_TRANSFER_START = 20;
-const int HBLANK_START = 63;
-const int VBLANK_START = 144;
+const int PIXEL_TRANSFER_START_CYCLE = 20;
+const int VBLANK_START_LINE = 144;
 const int LINE_LENGTH_NUM_CYCLES = 114 * 2;
 const int NUM_LINES = 154;
 
@@ -78,11 +80,120 @@ void Disable()
 	SDL_RenderPresent(sdl_renderer);
 }
 
+enum class PALLETTE_SOURCE
+{
+	BG,
+	S0,
+	S1,
+};
+
+struct FifoPixel
+{
+	u8 pallette_color;
+	PALLETTE_SOURCE pallette_source;
+};
+void WritePixel(FifoPixel pixel)
+{
+	// todo apply the pallette
+	*sdl_pixels_write++ = (current_h_cycle % 3) == 0 ? 255 : 0;
+	*sdl_pixels_write++ = (current_h_cycle % 3) == 1 ? 255 : 0;
+	*sdl_pixels_write++ = (current_h_cycle % 3) == 2 ? 255 : 0;
+	*sdl_pixels_write++ = 255;
+}
+
+static std::queue<FifoPixel> pixel_fifo;
+void StepFifo()
+{
+	if (pixel_fifo.size() > 8)
+	{
+		auto pixel = pixel_fifo.front();
+		pixel_fifo.pop();
+
+		WritePixel(pixel);
+	}
+	// todo start hblank when we've written out a line.
+}
+
+enum class FETCH_MODE
+{
+	BACKGROUND,
+	SPRITE,
+};
+static FETCH_MODE fetch_mode = FETCH_MODE::BACKGROUND;
+enum class FETCH_STATE
+{
+	READ_TILE_NUMBER = 0,
+	READ_TILE_NUMBER_IDLE,
+	READ_DATA_ZERO,
+	READ_DATA_ZERO_IDLE,
+	READ_DATA_ONE,
+	READ_DATA_ONE_IDLE,
+	WRITE_TO_FIFO_IDLE,
+	WRITE_TO_FIFO
+};
+static FETCH_STATE fetch_state = FETCH_STATE::READ_TILE_NUMBER;
+static u16 fetch_source_address;
+void StepFetch()
+{
+	switch (fetch_mode)
+	{
+	case FETCH_MODE::BACKGROUND:
+	{
+		switch (fetch_state)
+		{
+		case FETCH_STATE::READ_TILE_NUMBER:
+		{
+
+		}
+		break;
+		case FETCH_STATE::READ_DATA_ZERO:
+		{
+		}
+		break;
+		case FETCH_STATE::READ_DATA_ONE:
+		{
+		}
+		break;
+		case FETCH_STATE::READ_DATA_ONE_IDLE:
+		{
+			if (pixel_fifo.size() == 8)
+			{
+				goto label_write_to_fifo;
+			}
+		}
+		break;
+		case FETCH_STATE::WRITE_TO_FIFO:
+		{
+		label_write_to_fifo:
+			void(0);
+			//assert(pixel_fifo.size() == 8);
+			// todo move pixels into fifo
+		}
+		break;
+		}
+
+		fetch_state = static_cast<FETCH_STATE>(static_cast<int>(fetch_state)+1);
+	}
+	break;
+	}
+}
+
+u16 GetBackgroundMapStartAddr()
+{
+	u8 lcdc_reg = Memory::LoadU8((u16)SpecialRegister::VIDEO_LCDC);
+	const bool mode_1 = lcdc_reg & (u8)LCD_CONTROL_FLAGS::BACKGROUND_MAP_ADDR;
+	return mode_1 ? (u16)AddressRegion::BACKGROUND_TILE_MAP_MODE_1_START : (u16)AddressRegion::BACKGROUND_TILE_MAP_MODE_0_START;
+}
+
+bool IsPpuEnabled()
+{
+	u8 lcdc_reg = Memory::LoadU8((u16)SpecialRegister::VIDEO_LCDC);
+	return lcdc_reg & (u8)LCD_CONTROL_FLAGS::POWER;
+}
+
 void PPU::Step()
 {
-	// Handle being disabled
-	u8 lcdc_reg = Memory::LoadU8((u16)SpecialRegister::VIDEO_LCDC);
-	if ((lcdc_reg & (u8)LCDC_FLAGS::LCD_POWER) == 0)
+	if (!IsPpuEnabled())
 	{
 		if (ppu_state != PPU_STATE::DISABLED)
 		{
@@ -107,7 +218,7 @@ void PPU::Step()
 	}
 
 	// Set state
-	if (InRange(ly_reg, VBLANK_START, NUM_LINES))
+	if (InRange(ly_reg, VBLANK_START_LINE, NUM_LINES))
 	{
 		ppu_state = PPU_STATE::VBLANK;
 	}
@@ -115,13 +226,26 @@ void PPU::Step()
 	{
 		ppu_state = PPU_STATE::OAM_SEARCH;
 	}
-	else if (current_h_cycle == PIXEL_TRANSFER_START)
+	else if (current_h_cycle == PIXEL_TRANSFER_START_CYCLE)
 	{
 		ppu_state = PPU_STATE::PIXEL_TRANSFER;
+		fetch_state = FETCH_STATE::READ_TILE_NUMBER;
+		fetch_source_address = GetBackgroundMapStartAddr();
 	}
-	else if (current_h_cycle == HBLANK_START)
+
+	switch (ppu_state)
 	{
-		ppu_state = PPU_STATE::HBLANK;
-		// todo generate HBLANK interrupt
+	case PPU_STATE::OAM_SEARCH:
+	{
+		//todo
+	}
+	break;
+	case PPU_STATE::PIXEL_TRANSFER:
+	{
+		StepFifo();
+		StepFetch();
+	}
+	break;
+
 	}
 }
